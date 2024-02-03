29c7b18db2573fadba9eb467851b8a5d8f6cd04a
Md Sahadot islam <sahadot001s@gmail.com>
2024-02-03 10:23:55 +0600

    aa

---
+++ input.js
@@ -0,0 +1,168 @@
+{
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        autoplay: {
+          delay: 1500,
+        },
+        // navigation: {
+        //   nextEl: '.swiper-button-next',
+        //   prevEl: '.swiper-button-prev',
+        // },
+        breakpoints: {
+          1200: {
+              slidesPerView: 4,
+              spaceBetweenSlides: 0
+          },
+          992: {
+              slidesPerView: 3,
+              spaceBetweenSlides: 20
+          },
+          668: {
+            slidesPerView: 2,
+            spaceBetweenSlides: 20
+        },
+        0: {
+          slidesPerView: 1,
+          spaceBetweenSlides: 20
+      }
+      }
+      });
+}
+
+{
+      var swiper = new Swiper(".mySwiper1", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        autoplay: {
+          delay: 1500,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next1",
+          prevEl: ".swiper-button-prev1",
+        },
+      });
+}
+
+{
+    var swiper = new Swiper(".slid3i", {
+        slidesPerView: 6,
+        spaceBetween: 10,
+        autoplay: {
+          delay: 1500,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+          1920: {
+            slidesPerView: 6,
+            spaceBetweenSlides: 0
+        },
+          1400: {
+              slidesPerView: 5,
+              spaceBetweenSlides: 0
+          },
+          992: {
+              slidesPerView: 4,
+              spaceBetweenSlides: 20
+          },
+          668: {
+            slidesPerView: 3,
+            spaceBetweenSlides: 20
+        },
+        0: {
+          slidesPerView: 1,
+          spaceBetweenSlides: 20
+      }
+      }
+      });
+}
+
+{
+  var swiper = new Swiper(".mySwiper6", {
+      slidesPerView: 6,
+      spaceBetween: 10,
+      autoplay: {
+        delay: 1500,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        1920: {
+          slidesPerView: 6,
+          spaceBetweenSlides: 0
+      },
+      1400: {
+        slidesPerView: 5,
+        spaceBetweenSlides: 0
+      },
+        1200: {
+            slidesPerView: 4,
+            spaceBetweenSlides: 0
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetweenSlides: 20
+        },
+        668: {
+          slidesPerView: 3,
+          spaceBetweenSlides: 20
+      },
+      0: {
+        slidesPerView: 1,
+        spaceBetweenSlides: 20
+    }
+    }
+    });
+}
+
+// {
+//   const swiper = new Swiper('.swiper3', {
+//     direction: 'vertical',
+//     loop: true,
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     }
+//   });
+// }
+{
+  var swiper = new Swiper(".swiper3", {
+      slidesPerView: 4,
+      spaceBetween: 10,
+      autoplay: {
+        delay: 1500,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      // navigation: {
+      //   nextEl: '.swiper-button-next',
+      //   prevEl: '.swiper-button-prev',
+      // },
+      breakpoints: {
+        1200: {
+            slidesPerView: 4,
+            spaceBetweenSlides: 0
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetweenSlides: 20
+        },
+        668: {
+          slidesPerView: 2,
+          spaceBetweenSlides: 20
+      },
+      0: {
+        slidesPerView: 1,
+        spaceBetweenSlides: 20
+    }
+    }
+    });
+  }
